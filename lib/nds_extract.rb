a2c64f12799d4f87680f8fd2d0b11fafb546b3ca
Todd Carlson <todd.carlson3030@me.com>
2019-12-24 07:38:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,9 +11,7 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-    
-  }
+  
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
@@ -25,11 +23,26 @@ def directors_totals(nds)
   # Be sure to return the result at the end!
   #nds[0] = "Stephen Spielberg"
   #nds[0][:movies][0][:worldwide_gross] = gross of Jaws #=> 260000000
-  key = 0 
-  value = 0 
-  name = nds[0]
-  gross = nds[0][:movies][0][:worldwide_gross]
-  while value < name.length do 
-    
-  end
+  total = {}
+    row_index = 0 
+
+
+  while row_index < nds.length do
+    column = 0
+        director_name = nds[row_index][:name]
+          movies = nds[row_index][:movies]
+        #binding.pry
+   while column < movies.length
+
+        if !total[director_name]
+          total[director_name] = movies[column][:worldwide_gross]
+         else
+          total[director_name] += movies[column][:worldwide_gross]
+
+       end
+       column += 1
+     end
+    row_index += 1    
+   end
+ return total
 end
